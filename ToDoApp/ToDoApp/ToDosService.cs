3a6be7482fe360c21de11405b0b3699af312e3a6
FilipDavid1 <filipenkodavid@gmail.com>
2021-01-11 17:36:56 +0100

    create ToDosService class

--- ToDoApp/ToDoApp/ToDosService.cs
+++ ToDoApp/ToDoApp/ToDosService.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using ToDoApp.Models;
 
 namespace ToDoApp
 {
     class ToDosService
     {
+
+
+        string filePath;
+
+        public ObservableCollection<ToDo> ToDos { get; private set; }
+
+        public ToDosService(string filePath)
+        {
+            this.filePath = filePath;
+            ToDos = new ObservableCollection<ToDo>();
+        }
+
     }
 }
