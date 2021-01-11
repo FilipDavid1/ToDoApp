859ffc969217ce10dc29145573e7d4d585beb959
FilipDavid1 <filipenkodavid@gmail.com>
2021-01-11 18:21:09 +0100

    add metod SaveToDos in which will be smart class XmlSerializer- class for saving to format XML

--- ToDoApp/ToDoApp/ToDosService.cs
+++ ToDoApp/ToDoApp/ToDosService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Text;
+using System.Xml.Serialization;
 using ToDoApp.Models;
 
 namespace ToDoApp
@@ -19,6 +22,17 @@ namespace ToDoApp
             this.filePath = filePath;
             ToDos = new ObservableCollection<ToDo>();
         }
+
+        public void SaveToDos()
+        {
+            XmlSerializer serializer = new XmlSerializer(ToDos.GetType());
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                serializer.Serialize(sw, ToDos);
+            }
+        }
+
+        
 
     }
 }
